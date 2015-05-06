04682261aa6e26776c4015b09a9ed98bc9d831d0
etienne <etienne.desautels@gmail.com>
2015-05-06 13:30:43 -0400

    Add base of Voiture model.

--- cody/voiture/models.py
+++ cody/voiture/models.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Voiture(models.Model):
+    # Could be linked to a Marque table instead if necessary.
+    marque = models.CharField(max_length=64)
+    annee = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    cylindree = models.CharField(max_length=32, blank=True)
+    # Could be split in surname, name or linked to a Proprietaire table
+    # if necessary.
+    proprietaire = models.CharField(max_length=64)
+    # Here we split photos in one directory each month. Can be change to day,
+    # etc. in function of the volume.
+    photo = models.ImageField(upload_to='photos/%Y-%m', blank=True)
+
+    class Meta:
+        verbose_name = u'Voiture'
+        verbose_name_plural = u'Voitures'
+
+    def __unicode__(self):
+        return (u'{self.marque} ({self.annee}) '
+                u'- Propriétaire : {self.proprietaire}').format(self=self)
