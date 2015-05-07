0d47a4389502d5cbaa3eb17cbfecd9e7c29051eb
etienne <etienne.desautels@gmail.com>
2015-05-07 10:25:48 -0400

    Add basic list and detail voiture views.

--- cody/voiture/views.py
+++ cody/voiture/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from rest_framework import generics
+
+from voiture.models import Voiture
+from voiture.serializers import VoitureSerializer
+
+
+class VoitureList(generics.ListCreateAPIView):
+    queryset = Voiture.objects.all()
+    serializer_class = VoitureSerializer
+
+
+class VoitureDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Voiture.objects.all()
+    serializer_class = VoitureSerializer
