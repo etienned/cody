fc9786fa0e9f7ef7bd2e9a1afcac85676173506f
etienne <etienne.desautels@gmail.com>
2015-05-07 12:01:20 -0400

    Add test for getting a voiture.

--- cody/voiture/tests.py
+++ cody/voiture/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class VoitureTests(APITestCase):
+    fixtures = ['voitures.json']
+
+    def test_get_voiture(self):
+        """
+        Ensure we can get a existing voiture object.
+        """
+        url = reverse('voiture:detail', args=(1,))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'cylindree': u'1,8 l',
+            'description': u'',
+            'photo': None,
+            'marque': u'Renault',
+            'proprietaire': u'Tanguy de Montigny',
+            'annee': None,
+            'id': 1
+        })
