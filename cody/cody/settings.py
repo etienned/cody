79e4dd6e1ec2111b775e6a82141d909b8a0af1c3
etienne <etienne.desautels@gmail.com>
2015-05-07 11:36:39 -0400

    Add JSON as default format for REST tests.

--- cody/cody/settings.py
+++ cody/cody/settings.py
@@ -102,3 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST API
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
